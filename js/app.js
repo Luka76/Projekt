e560a60a621419df87cbf94a8760c0d901f18f79
Luka76 <vargaluka76@gmail.com>
2022-05-17 11:34:25 +0200

    exported class from app

--- js/app.js
+++ js/app.js
@@ -1,83 +1,12 @@
 'use strict';
+import {Slider} from './exportedClass'
 
-class Slider {
-    #slider;
-    #rightButton;
-    #leftButton;
-
-    constructor(slider, leftButton, rightButton) {
-        this.#slider = slider;
-        this.#rightButton = rightButton;
-        this.#leftButton = leftButton;
-
-        this.#rightButton.on('click', this.next.bind(this))
-        this.#leftButton.on('click', this.previous.bind(this))
-        
-    }
-
-    previous() {
-        if (this.#isTransitionInProgress()) {
-            return;
-        }
-
-        const first = this.#firstImage();
-        const duplicated = first.cloneNode(true);
-
-        this.#insertElement(duplicated, 'end')
-        this.#removeElement(first)
-    }
-
-    next() {
-        if (this.#isTransitionInProgress()) {
-            return;
-        }
-
-        const last = this.#lastImage();
-        const duplicated = last.cloneNode(true);
-
-        this.#removeElement(last)
-        this.#insertElement(duplicated)
-    }
-
-    #isTransitionInProgress() {
-        return $(this.#slider).find('.fade-in,.fade-out').length > 0
-    }
-
-    #lastImage() {
-        return this.#slider.lastElementChild;
-    }
-
-    #firstImage() {
-        return this.#slider.firstElementChild;
-    }
-
-    #insertElement(el, position = 'start') {
-        $(el).addClass('fade-in');
-
-        if (position === 'start') {
-            this.#slider.prepend(el);
-        } else {
-            this.#slider.append(el)
-        }
-
-        $(el).on('animationend', (e) => {
-            $(el).removeClass('fade-in');
-        });
-    }
-
-    #removeElement(el) {
-        $(el).on('animationend', (e) => {
-            el.remove()
-        });
-
-        $(el).addClass('fade-out');
-    }
-}
-
+const jqFirstSlider = $('.slider')[0];
+const jqSecondSlider = $('.slider')[1];
 
 $(document).ready(function (){
     const slider1 = new Slider(
-        $('.slider')[0],
+        $(jqFirstSlider),
         $('#left-button'),
         $('#right-button')
         
@@ -85,7 +14,7 @@ $(document).ready(function (){
 
 
     const slider2 = new Slider(
-        $('.slider')[1],
+        $(jqSecondSlider),
         $('#left-button'),
         $('#right-button')
     );
